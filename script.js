f410263454fec50f14a7e246352dc5b65fd0d667
LawrieDrew <laurenn.drew@gmail.com>
2021-07-06 11:15:31 -0400

    grabbed api key from open weather map, pseudo coding

---
+++ script.js
@@ -0,0 +1,32 @@
+var city="";
+//city now
+//city forecast
+//temperature
+//uv index
+//humidity 
+//wind
+
+const city =
+const Date = moment().format('ddd MMMM DD' + ',' + 'YYYY');
+const temperature = document.querySelector('.temperature')
+const uvIndex =
+const humidity =
+const wind =
+const apiKey ='6e0a6f003c0ba257f116574ef04963ac';
+
+//api call somwehre 
+//fetchcall? 
+//URL: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={apiKey}
+
+
+
+var d = new Date();
+document.getElementById("").innerHTML = d;
+
+
+function todaysForecast();
+
+
+
+clearWatch()
+init();
