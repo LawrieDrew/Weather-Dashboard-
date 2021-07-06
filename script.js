7ab5968369043906ca5821b4a5831d149f4ade89
LawrieDrew <laurenn.drew@gmail.com>
2021-07-06 12:01:10 -0400

    mapping out constants

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-var city="";
 //city now
 //city forecast
 //temperature
 //uv index
 //humidity 
 //wind
+const card= $('<div>').addClass('card');
+const city = $('<h1>').addClass('card-title').text(response.name);
+const Date = moment('<h1>').format('ddd MMMM DD' + ',' + 'YYYY');
+const temperature = document.querySelector('.temperature');
+var uvIndex = $('#uv-index');
+//const uvIndex = $('').addClass('card-text current-uvIndex').text("Current UV Index: ")
+const humidity = $('').addClass('card-text current-humidity').text("It Feels Like: " + response.main.humidity + '%');
+const wind = $('').addClass('card-text current-wind').text("Feel the Breeze: " + response.wind.speed + " MPH");
 
-const city =
-const Date = moment().format('ddd MMMM DD' + ',' + 'YYYY');
-const temperature = document.querySelector('.temperature')
-const uvIndex =
-const humidity =
-const wind =
 const apiKey ='6e0a6f003c0ba257f116574ef04963ac';
-
+const queryURL = 'api.openweathermap.org/data/2.5/forecast?q=' + city + apiKey;
 //api call somwehre 
 //fetchcall? 
 //URL: api.openweathermap.org/data/2.5/forecast?q={city name}&appid={apiKey}
+//var d = new Date();
 
-
-
-var d = new Date();
 document.getElementById("").innerHTML = d;
 
 
@@ -28,5 +27,11 @@ function todaysForecast();
 
 
 
-clearWatch()
+function clearResults(){
+  event.preventDefault();
+  city=[];
+  localStorage.removeItem('city');
+  document.location.reload();
+
+}
 init();
